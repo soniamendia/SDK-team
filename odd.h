e5309cd1b999297cb84112f103c6b2cf5ec89831
kukul <2109093@upy.edu.mx>
2022-04-04 15:48:55 -0500

    odd library added

--- odd.h
+++ odd.h
@@ -1,7 +1,8 @@
 int odd (int array[], int len_array){
     int value;
-    for (int i = 0; <len_array[i]){
-        if (value%2 == 0) printf("%d/n", i);
+    for (int i = 0; i<len_array; i++){
+        value = array[i];
+        if (value%2 != 0) printf("%d ", i);
     }
 
 }
